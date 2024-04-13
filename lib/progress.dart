dd565f76c841d68c47bb61869ddf732a6ecc1a10
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-13 22:12:43 +0530

    feat: add firebase connection and basic implementation

--- lib/progress.dart
+++ lib/progress.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -9,8 +11,37 @@ class Progress extends StatefulWidget {
 }
 
 class _ProgressState extends State<Progress> {
+  final Future<FirebaseApp> _fbaseApp = Firebase.initializeApp();
+  final DatabaseReference _dbRef =
+      FirebaseDatabase.instance.ref().child('Sensor');
+
+  int current = 0;
+  int capacity = 0;
+  double calculatedValue = 0;
+  int percentage = 0;
+
+  double calculateValue() {
+    return (100 - ((current / capacity) * 100)) / 100;
+  }
+
+  void setOnce() {
+    setState(() {
+      capacity = current;
+      _dbRef.child('max_val').set(capacity);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    _dbRef.child("ultra_data").onValue.listen(
+      (event) {
+        setState(() {
+          current = event.snapshot.value as int;
+          calculatedValue = calculateValue();
+          percentage = (calculatedValue * 100).toInt();
+        });
+      },
+    );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[400],
@@ -26,21 +57,66 @@ class _ProgressState extends State<Progress> {
       body: Container(
         alignment: Alignment.center,
         color: Colors.blue[100],
-        child: CircularPercentIndicator(
-          radius: 130,
-          lineWidth: 20,
-          percent: 0.1,
-          progressColor: Colors.blue,
-          backgroundColor: Colors.blue.shade200,
-          circularStrokeCap: CircularStrokeCap.round,
-          center: const Text(
-            "10%",
-            style: TextStyle(
-              color: Colors.white70,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        child: FutureBuilder(
+          future: _fbaseApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text(
+                "Connection Error..!",
+                style: TextStyle(),
+              );
+            } else if (snapshot.hasData) {
+              return (capacity != 0)
+                  ? Column(
+                      children: [
+                        CircularPercentIndicator(
+                          radius: 130,
+                          lineWidth: 20,
+                          percent:
+                              (calculatedValue >= 0.0 && calculatedValue <= 1.0)
+                                  ? calculatedValue
+                                  : 0,
+                          progressColor: Colors.blue,
+                          backgroundColor: Colors.blue.shade200,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Text(
+                            "${(percentage >= 0 && percentage <= 100) ? percentage : 0}%",
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text("Current : $current"),
+                        Text("Capacity : $capacity"),
+                        Text("Calculated Value : $calculatedValue"),
+                      ],
+                    )
+                  : ElevatedButton(
+                      onPressed: () => setOnce(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue[300],
+                        padding: const EdgeInsets.all(18),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        "Set Maximum $current",
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
       ),
     );
