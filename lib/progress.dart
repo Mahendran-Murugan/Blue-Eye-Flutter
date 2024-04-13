72c107f1cc96754b1f341070c15e77201ad147c1
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-13 13:51:13 +0530

    feat: add progress for ui

---
+++ lib/progress.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Progress extends StatefulWidget {
+  const Progress({super.key});
+
+  @override
+  State<Progress> createState() => _ProgressState();
+}
+
+class _ProgressState extends State<Progress> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
